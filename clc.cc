6498a968cd34e49e355cc0765ca2dafb842bc12b
Adrian Kummerlaender <knairda@t-online.de>
2014-09-27 18:36:56 +0200

    Added argument and tree export mode to frontend * arguments are managed through boost::program_options * default continues to be the REPL * "--tree" argument switches from calculation to tree export * "--input" may be used to pass terms to be evaluated via CLI arguments ** disables REPL

--- clc.cc
+++ clc.cc
@@ -1,22 +1,122 @@
+#include <boost/optional.hpp>
+#include <boost/program_options.hpp>
+
 #include <iostream>
 #include <limits>
 
 #include "src/parser.h"
 
-typedef std::numeric_limits<double> dbl;
+boost::optional<boost::program_options::variables_map> input(
+	int argc,
+	char** argv
+) {
+	boost::program_options::options_description optionDescription(
+		"Supported options"
+	);
 
-int main() {
-	std::string inputTerm;
-	std::cout.precision(dbl::digits10);
-	
-	while ( std::cin >> inputTerm ) {
-		try {
-			std::cout << SimpleParser::calculate(inputTerm) << std::endl;
-		}
-		catch ( std::exception &e ) {
-			std::cerr << e.what() << std::endl;
-		}
+	optionDescription.add_options()
+		(
+			"input",
+			boost::program_options::value<std::vector<std::string>>(),
+			"terms to be evaluated"
+		)
+		(
+			"tree",
+			boost::program_options::value<bool>()->implicit_value(true),
+			"return the binary expression tree instead of evaluating"
+		)
+	;
+
+	boost::program_options::variables_map variables;
+
+	try {
+		boost::program_options::store(
+			boost::program_options::parse_command_line(
+				argc, argv, optionDescription
+			),
+			variables
+		);
+
+		boost::program_options::notify(variables);
+	}
+	catch ( const std::exception& exception ) {
+		std::cerr << exception.what()  << std::endl;
+		std::cout << optionDescription << std::endl;
+
+		return boost::optional<boost::program_options::variables_map>();
 	}
 
-	return 0;
+	return boost::make_optional(variables);
+}
+
+bool process(const boost::program_options::variables_map& variables) {
+	const bool replMode{ variables.count("input") == 0 };
+	const bool treeMode{ variables.count("tree") ? variables["tree"].as<bool>()
+	                                             : false };
+
+	if ( replMode ) {
+		std::cout.precision(
+			std::numeric_limits<double>::digits10
+		);
+
+		std::string term;
+
+		while ( std::cin >> term ) {
+			try {
+				if ( treeMode) {
+					std::cout << SimpleParser::print(term);
+				} else {
+					std::cout << SimpleParser::calculate(term);
+				}
+
+				std::cout << std::endl;
+			}
+			catch ( std::exception &exception ) {
+				std::cerr << exception.what() << std::endl;
+			}
+		}
+
+		return true;
+	} else {
+		const std::vector<std::string> inputTerms{
+			variables["input"].as<std::vector<std::string>>()
+		};
+
+		try {
+			std::for_each(
+				inputTerms.cbegin(),
+				inputTerms.cend(),
+				[treeMode](const std::string& term) {
+					if ( treeMode) {
+						std::cout << SimpleParser::print(term);
+					} else {
+						std::cout << term
+						          << " = "
+						          << SimpleParser::calculate(term);
+					}
+
+					std::cout << std::endl;
+				}
+			);
+		}
+		catch ( std::exception &exception ) {
+			std::cerr << exception.what() << std::endl;
+
+			return false;
+		}
+
+		return true;
+	}
+}
+
+int main(int argc, char** argv) {
+	if ( auto variables = input(argc, argv) ) {
+		if ( process(*variables) ) {
+			return 0;
+		} else {
+			return 1;
+		}
+	} else {
+		return 1;
+	}
 }
